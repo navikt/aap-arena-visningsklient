99c047105072ad7d1f1e39c049b5859904e54d58
Steffen Hageland <steffen.hageland@nav.no>
2026-03-11 14:52:53 +0100

    Nyt tabs på forsiden

--- app/sak/[saksId]/sak-page-client.tsx
+++ app/sak/[saksId]/sak-page-client.tsx
@@ -24,23 +24,22 @@ export function SakPageClient({ sak }: Props): React.ReactElement {
         <div>
           <Tabs defaultValue="sak">
             <Tabs.List>
-              <Tabs.Tab value="sak" label="Sak" />
-              <Tabs.Tab value="meldekortperioder" label="Meldekortperioder" />
+              <Tabs.Tab value="sak" label={`Sak ${sak.opprettetAar} ${sak.lopenr}`} />
+              <Tabs.Tab value="tilkjent-ytelse" label="Tilkjent ytelse" />
+              <Tabs.Tab value="spesialutbetaling" label="Spesialutbetaling" />
+              <Tabs.Tab value="notater" label="Notater" />
             </Tabs.List>
             <Tabs.Panel value="sak">
               <div className={styles.tabcontent}>
                 <Sakogvedtakinfo sak={sak} />
               </div>
             </Tabs.Panel>
-            <Tabs.Panel value="meldekortperioder">
+            <Tabs.Panel value="tilkjent-ytelse">
               <div className={styles.tabcontent}>
                 <Meldekortperioder />
               </div>
             </Tabs.Panel>
           </Tabs>
-        </div>
-        <div>
-          <Card>Her kan det komme informasjon om f.eks. kvoter</Card>
         </div>
       </div>
     </>
