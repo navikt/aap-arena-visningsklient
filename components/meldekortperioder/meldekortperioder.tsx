1c449fb3ffac9afc02864584700a5eda191471ef
Steffen Hageland <steffen.hageland@nav.no>
2026-03-13 11:22:22 +0100

    Push til prod?! (#14)
    * Litt bedre feilside på tabs
    
    * Push til prod ved merge!
    
    * Fikse eslintfeil

--- components/meldekortperioder/meldekortperioder.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { Card } from 'components/felleskomponenter/card/Card';
-
-export function Meldekortperioder(): React.ReactElement {
-  return <Card>Her skal det kommer informasjon om meldekortperioder</Card>;
-}
