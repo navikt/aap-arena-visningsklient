2c7b49a378a003c8802c0f2036341a2a4abf7766
Steffen Hageland <steffen.hageland@nav.no>
2026-03-13 11:13:05 +0100

    Legger til aktivitetsfase som kolonne og ikon på utfall (#15)

--- components/sakogvedtakinfo/vedtak-tabell.tsx
+++ components/sakogvedtakinfo/vedtak-tabell.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import styles from './sakogvedtak.module.css';
 import { ArenaVedtakMedFaktaDTO } from 'lib/services/arenaoppslag/arenaoppslag-types';
-import { BodyShort, Heading, Table } from '@navikt/ds-react';
+import { BodyShort, Heading, HStack, Table } from '@navikt/ds-react';
 import { format } from 'date-fns';
 import { Vedtakfakta } from 'components/sakogvedtakinfo/vedtakfakta';
 import { dateComperator, parseISOorNull } from 'lib/utils/date';
+import { CheckmarkCircleIcon } from '@navikt/aksel-icons';
+import { XMarkOctagonIcon } from '@navikt/aksel-icons';
 
 type Props = {
   vedtak: ArenaVedtakMedFaktaDTO[];
@@ -32,9 +35,10 @@ export function VedtakTabell({ vedtak }: Props): React.ReactElement {
           <Table.Row>
             <Table.HeaderCell scope="col">Rettighetskode</Table.HeaderCell>
             <Table.HeaderCell scope="col">Vedtakstype</Table.HeaderCell>
+            <Table.HeaderCell scope="col">Aktivitetsfase</Table.HeaderCell>
             <Table.HeaderCell scope="col">Fra og med</Table.HeaderCell>
             <Table.HeaderCell scope="col">Til og med</Table.HeaderCell>
-            <Table.HeaderCell scope="col">Status</Table.HeaderCell>
+            <Table.HeaderCell scope="col">Behandslingsstatus</Table.HeaderCell>
             <Table.HeaderCell scope="col">Utfall</Table.HeaderCell>
             <Table.HeaderCell scope="col"></Table.HeaderCell>
           </Table.Row>
@@ -45,6 +49,7 @@ export function VedtakTabell({ vedtak }: Props): React.ReactElement {
               rettighetkode,
               vedtaktypeNavn,
               vedtaktypeKode,
+              aktivitetsfaseNavn,
               fraOgMed,
               tilDato,
               statusNavn,
@@ -60,10 +65,13 @@ export function VedtakTabell({ vedtak }: Props): React.ReactElement {
                 >
                   <Table.HeaderCell scope="row">{rettighetkode}</Table.HeaderCell>
                   <Table.DataCell>{`${vedtaktypeNavn} (${vedtaktypeKode})`}</Table.DataCell>
+                  <Table.DataCell>{aktivitetsfaseNavn}</Table.DataCell>
                   <Table.DataCell>{dateOrBlank(fraOgMed)}</Table.DataCell>
                   <Table.DataCell>{dateOrBlank(tilDato)}</Table.DataCell>
                   <Table.DataCell>{statusNavn}</Table.DataCell>
-                  <Table.DataCell>{utfallkode}</Table.DataCell>
+                  <Table.DataCell>
+                    <JaNeiStatus status={utfallkode} />
+                  </Table.DataCell>
                 </Table.ExpandableRow>
               );
             }
@@ -79,4 +87,32 @@ function dateOrBlank(datostring: string | null | undefined): string {
     return '—';
   }
   return format(new Date(datostring), 'dd.MM.yyyy');
+}
+
+type JaNeiStatusProps = {
+  status: string | null | undefined;
+};
+
+function JaNeiStatus({ status }: JaNeiStatusProps): React.ReactElement {
+  if (status == null) {
+    return <div>—</div>;
+  }
+  if (status.toLowerCase() === 'ja') {
+    return (
+      <HStack align="center" gap="space-4">
+        <CheckmarkCircleIcon className={styles.successIcon} />
+        <BodyShort size="medium">Ja</BodyShort>
+      </HStack>
+    );
+  }
+  if (status.toLowerCase() === 'nei') {
+    return (
+      <HStack align="center" gap="space-4">
+        <XMarkOctagonIcon className={styles.errorIcon} />
+        <BodyShort size="medium">Nei</BodyShort>
+      </HStack>
+    );
+  }
+
+  return <div>{status}</div>;
 }
