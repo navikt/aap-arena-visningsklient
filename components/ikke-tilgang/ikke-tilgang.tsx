0c31bdeafd6b48c00ca2f2c3b8965afd6d34bc94
Steffen Hageland <steffen.hageland@nav.no>
2026-04-28 11:01:29 +0200

    Oppdater feilmeldinger og design for ikke-tilgang og sak-ikke-funnet

--- components/ikke-tilgang/ikke-tilgang.tsx
+++ components/ikke-tilgang/ikke-tilgang.tsx
@@ -1,20 +1,25 @@
 'use client';
 
+import styles from './ikke-tilgang.module.css';
 import { GlobalAlert } from '@navikt/ds-react';
 
 type Props = {
-  brukerId: string;
+  saklopenummer: number;
+  sakaar: number;
 };
 
-export function IkkeTilgang({ brukerId }: Props): React.ReactElement {
+export function IkkeTilgang({ saklopenummer, sakaar }: Props): React.ReactElement {
   return (
-    <GlobalAlert status="error">
-      <GlobalAlert.Header>
-        <GlobalAlert.Title>Ikke tilgang til bruker (WIP feilmelding)</GlobalAlert.Title>
-      </GlobalAlert.Header>
-      <GlobalAlert.Content>
-        Du har ikke tilgang til bruker med fødselsnummer '{brukerId}'. Om dette er feil kan du ta kontakt med...
-      </GlobalAlert.Content>
-    </GlobalAlert>
+    <div className={styles.wrapper}>
+      <GlobalAlert className={styles.alert} status="warning" size="medium">
+        <GlobalAlert.Header>
+          <GlobalAlert.Title>Ikke tilgang til sak</GlobalAlert.Title>
+        </GlobalAlert.Header>
+        <GlobalAlert.Content>
+          Du har ikke tilgang til bruker på sak {sakaar} {saklopenummer}. Hvis du mener dette er feil, kan du ta kontakt
+          med brukerstøtte.
+        </GlobalAlert.Content>
+      </GlobalAlert>
+    </div>
   );
 }
