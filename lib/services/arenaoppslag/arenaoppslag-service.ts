61d26bb11dc02937002a54d0b2f587ea991a64f9
Simen Jensen <Simen.Andre.Dahl.Jensen@nav.no>
2026-08-18 09:43:18 +0200

    Refactor arenaoppslag-service.ts for improved code formatting and consistency

--- lib/services/arenaoppslag/arenaoppslag-service.ts
+++ lib/services/arenaoppslag/arenaoppslag-service.ts
@@ -1,29 +1,29 @@
 'use server';
 
-import {apiFetch} from 'lib/services/api-fetch/apiFetch';
-import {isError} from 'lib/utils/api';
-import {mocksEnabled} from 'lib/utils/environment';
-import {SakDTO} from 'lib/services/arenaoppslag/arenaoppslag-types';
-import {getLogger} from 'lib/serverutlis/logger';
-import {getMockSakFraArena} from 'lib/services/arenaoppslag/arenaoppslag-mock';
+import { apiFetch } from 'lib/services/api-fetch/apiFetch';
+import { isError } from 'lib/utils/api';
+import { mocksEnabled } from 'lib/utils/environment';
+import { SakDTO } from 'lib/services/arenaoppslag/arenaoppslag-types';
+import { getLogger } from 'lib/serverutlis/logger';
+import { getMockSakFraArena } from 'lib/services/arenaoppslag/arenaoppslag-mock';
 
 const baseUrl = process.env.ARENAOPPSLAG_API_BASE_URL;
 const scope = process.env.ARENAOPPSLAG_API_SCOPE || '';
 const logger = getLogger('lib.services.arenaoppslag');
 
 export async function hentSak(saksId: string): Promise<SakDTO | null> {
-    if (mocksEnabled()) {
-        return getMockSakFraArena(saksId);
-    }
+  if (mocksEnabled()) {
+    return getMockSakFraArena(saksId);
+  }
 
-    const response = await apiFetch<SakDTO>(`${baseUrl}/api/intern/sak/${saksId}/detaljert`, scope, 'GET');
-    if (isError(response)) {
-        if (response.status === 404) {
-            return null;
-        }
-        logger.error('Noe gikk galt ved henting av sak fra arenaoppslag', {response});
-        throw Error('Noe gikk galt ved henting av sak fra arenaoppslag', {cause: response});
+  const response = await apiFetch<SakDTO>(`${baseUrl}/api/intern/sak/${saksId}/detaljert`, scope, 'GET');
+  if (isError(response)) {
+    if (response.status === 404) {
+      return null;
     }
+    logger.error('Noe gikk galt ved henting av sak fra arenaoppslag', { response });
+    throw Error('Noe gikk galt ved henting av sak fra arenaoppslag', { cause: response });
+  }
 
-    return response.data;
+  return response.data;
 }
