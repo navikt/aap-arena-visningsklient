885f20428b3b3d5c2423c51de49c63f9cf3c15d9
Simen Jensen <Simen.Andre.Dahl.Jensen@nav.no>
2026-09-09 14:29:10 +0200

    Refactor TilkjentYtelsePage to streamline data fetching logic

--- app/sak/[saksId]/tilkjent-ytelse/page.tsx
+++ app/sak/[saksId]/tilkjent-ytelse/page.tsx
@@ -4,10 +4,7 @@ import { TilkjentYtelse } from 'components/tilkjent-ytelse/tilkjent-ytelse';
 export default async function TilkjentYtelsePage(props: { params: Promise<{ saksId: string }> }) {
   const { saksId } = await props.params;
 
-  const [sak, tilkjentYtelse] = await Promise.all([
-      hentSakHvisTilgang(saksId),
-      hentTilkjentYtelseHvisTilgang(saksId)]
-  );
+  const [sak, tilkjentYtelse] = await Promise.all([hentSakHvisTilgang(saksId), hentTilkjentYtelseHvisTilgang(saksId)]);
   if (sak == null) {
     return null;
   }
